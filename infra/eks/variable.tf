e6a54f2c04c3c7e769942693565818731dadeb55
sabaramzan123 <sabaramzan0226@gmail.com>
2026-05-08 09:06:49 +0500

    added files

---
+++ infra/eks/variable.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "project_name" {
+  type    = string
+  default = "eks-commerce"
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "ecommerce-cluster"
+}
