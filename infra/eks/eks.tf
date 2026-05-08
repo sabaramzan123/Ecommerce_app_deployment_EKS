2b3862245f672fb9c8a8fd9b2c1f75d1b29fad81
sabaramzan123 <sabaramzan0226@gmail.com>
2026-05-08 11:13:05 +0500

    fix: use public subnets, remove NAT gateway, increase timeout

--- infra/eks/eks.tf
+++ infra/eks/eks.tf
@@ -1,5 +1,3 @@
-# ─── VPC 
-
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 5.0"
@@ -7,24 +5,17 @@ module "vpc" {
   name = "${var.project_name}-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["${var.aws_region}a", "${var.aws_region}b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  azs            = ["${var.aws_region}a", "${var.aws_region}b"]
+  public_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
 
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
+  enable_nat_gateway   = false
   enable_dns_hostnames = true
 
   public_subnet_tags = {
     "kubernetes.io/role/elb" = "1"
   }
-
-  private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = "1"
-  }
 }
 
-# ─── EKS CLUSTER ─────
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
@@ -33,20 +24,23 @@ module "eks" {
   cluster_version = "1.31"
 
   vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  subnet_ids = module.vpc.public_subnets
 
   cluster_endpoint_public_access = true
 
-  # KMS encryption disable karo
-  create_kms_key            = false
-  cluster_encryption_config = {}
+  create_kms_key             = false
+  cluster_encryption_config  = {}
+  cluster_enabled_log_types  = []
 
   eks_managed_node_groups = {
     default = {
-      min_size       = 2
-      max_size       = 3
-      desired_size   = 2
+      min_size       = 1
+      max_size       = 2
+      desired_size   = 1
       instance_types = ["t3.medium"]
+
+      # Public subnets mein nodes
+      subnet_ids = module.vpc.public_subnets
     }
   }
 
