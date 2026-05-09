f955b9b7d64a9e67f5c976f9043729670a23ae12
sabaramzan123 <sabaramzan0226@gmail.com>
2026-05-09 08:26:57 +0500

    test deploy

--- infra/eks/eks.tf
+++ infra/eks/eks.tf
@@ -22,7 +22,7 @@ module "eks" {
   version = "~> 20.0"
 
   cluster_name    = var.cluster_name
-  cluster_version = "1.31"
+  cluster_version = "1.32"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.public_subnets
