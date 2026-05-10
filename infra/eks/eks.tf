beca7a9a87779d7ca1e907df642a95e09de2a651
sabaramzan123 <sabaramzan0226@gmail.com>
2026-05-10 16:50:30 +0500

    feat: add EBS CSI driver for StatefulSet PVC provisioning

--- infra/eks/eks.tf
+++ infra/eks/eks.tf
@@ -1,3 +1,22 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "${var.project_name}-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs            = ["${var.aws_region}a", "${var.aws_region}b"]
+  public_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway      = false
+  enable_dns_hostnames    = true
+  map_public_ip_on_launch = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+  }
+}
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
@@ -16,7 +35,7 @@ module "eks" {
 
   enable_cluster_creator_admin_permissions = true
 
-  # ← EBS CSI Driver add karo
+   # ← EBS CSI Driver add karo
   cluster_addons = {
     aws-ebs-csi-driver = {
       most_recent              = true
@@ -49,22 +68,5 @@ module "eks" {
   tags = {
     Project = var.project_name
   }
-  
-  
-}
 
-# ← EBS CSI Driver IAM Role
-module "ebs_csi_irsa" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  version = "~> 5.0"
-
-  role_name             = "${var.cluster_name}-ebs-csi-driver"
-  attach_ebs_csi_policy = true
-
-  oidc_providers = {
-    main = {
-      provider_arn               = module.eks.oidc_provider_arn
-      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
-    }
-  }
 }
